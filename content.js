4d920538e8f6e6a41e79110976d0173d7017156c
yilin3172-cloud <yilin3172-cloud@users.noreply.github.com>
2026-09-18 18:47:50 +0800

    Add organization team introductions

--- content.js
+++ content.js
@@ -7,11 +7,11 @@ window.INVITATION_CONFIG = {
   members: [
     { name: "罗子威学长", role: "团委团副", intro: "我是从组织部一路成长上来的罗子威，期待大家在新的校园中担当新的青春责任，祝前途光明！学业顺利！组织部欢迎你", avatar: "assets/luo-ziwei.jpg" },
     { name: "洪孜熙学姐", role: "团委兼职团干", intro: "我是行知书院团委的洪孜熙，愿与各位在团务实践中淬炼青春、收获成长，祝大家步履生辉，学业有成！团委组织部欢迎你的到来。", avatar: "assets/hong-zixi.jpg" },
-    { name: "李巧玲学姐", role: "组织部执行部长", intro: "", avatar: "assets/li-qiaoling.jpg" },
-    { name: "肖乐学姐", role: "组织部执行副部长", intro: "", avatar: "assets/xiao-le.jpg" },
-    { name: "刘宜佳学姐", role: "组织部部长", intro: "", avatar: "assets/liu-yijia.jpg" },
-    { name: "余雨扬学姐", role: "组织部副部长", intro: "", avatar: "assets/yu-yuyang.jpg" },
-    { name: "李俊睿学长", role: "组织部副部长", intro: "", avatar: "assets/li-junrui.jpg" },
-    { name: "吴依琳学姐", role: "组织部副部长", intro: "", avatar: "assets/wu-yilin.jpg" }
+    { name: "李巧玲学姐", role: "组织部执行部长", intro: "我是组织部执行部长李巧玲，很高兴与你相遇，热烈欢迎你的到来。期待接下来和你一同成长。", avatar: "assets/li-qiaoling.jpg" },
+    { name: "肖乐学姐", role: "组织部执行副部长", intro: "大家好！我是肖乐，非常高兴能在团委组织部与大家相遇。新的阶段，新的起点，愿我们携手并进、共同成长，一起成为更好的自己。祝大家学业顺利，万事顺遂！", avatar: "assets/xiao-le.jpg" },
+    { name: "刘宜佳学姐", role: "组织部部长", intro: "大家好！我是刘宜佳，很开心和大家相聚组织部，愿我们携手共同努力，在团务工作中踏实前行，同心共进，不负热爱，共赴新程！", avatar: "assets/liu-yijia.jpg" },
+    { name: "余雨扬学姐", role: "组织部副部长", intro: "大家好！我是组织部余雨扬。新征程新起点，希望我们相聚组织部，在团务工作的实践里锤炼自我，不负青春韶华！欢迎加入组织部大家庭！", avatar: "assets/yu-yuyang.jpg" },
+    { name: "李俊睿学长", role: "组织部副部长", intro: "大家好！我是李俊睿，很荣幸能在团委组织部与大家相遇。新阶段、新起点，愿我们同心协力、互促共进，在服务与奉献中共同成长。祝大家学有所成，诸事顺遂！", avatar: "assets/li-junrui.jpg" },
+    { name: "吴依琳学姐", role: "组织部副部长", intro: "大家好，我是组织部副部长吴依琳。欢迎各位同学的到来！期待接下来和大家一起协作，在组织部收获成长，也祝愿大家在这里有所收获。", avatar: "assets/wu-yilin.jpg" }
   ]
 };
